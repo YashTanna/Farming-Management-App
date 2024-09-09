b85406188dcbd273fb0054338e2737dd8e4c0a02
divy <divyvaghani1610@gmail.com>
2024-09-09 18:33:21 +0530

    "Implement introductory layout with app icon, dots, phone number section, category,description and scroll functionality"

---
+++ app/src/main/java/com/example/farmingmanagemant/FarmerIntroductionPage1.java
@@ -0,0 +1,20 @@
+package com.example.farmingmanagemant;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FarmerIntroductionPage1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_farmer_introduction_page1);
+
+    }
+}
